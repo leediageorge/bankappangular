83c59e02626f020d68bac1fb23bd67e20c3f6065
LeediaGeorge15 <varkey.able18@gmail.com>
2020-09-02 14:38:28 +0530

    added a comment on app module

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  accountDetails={
+    1001:{name:"user1",acno:1001,pin:4387,password:"userone",balance:3000},
+    1002:{name:"user2",acno:1002,pin:1234,password:"usertwo",balance:3500},
+1003:{name:"user3",acno:1003,pin:1598,password:"userthree",balance:4000},
+1004:{name:"user4",acno:1004,pin:1095,password:"userfour",balance:3050},
+1005:{name:"user5",acno:1005,pin:1098,password:"userfive",balance:300}
+
+}
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  login(){
+    alert("Login clicked");
   }
 
 }
